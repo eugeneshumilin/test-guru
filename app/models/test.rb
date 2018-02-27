6785575388bbf28d436166ce7255d96d3406bee0
eugeneshumilin <shumilinea@gmail.com>
2018-02-27 22:55:38 +0300

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :tests_user
+  has_many :users, through: :tests_user
+  belongs_to :author, class_name: 'User'
+  belongs_to :category
+
   def self.titles_by_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where(
       categories: { title: category }
--- db/migrate/20180224125004_create_users_tests.rb
+++ db/migrate/20180224125004_create_users_tests.rb
@@ -1,5 +1,8 @@
 class CreateUsersTests < ActiveRecord::Migration[5.1]
   def change
-    create_join_table :users, :tests
+    create_join_table :users, :tests do |t|
+      t.index [:user_id, :test_id], unique: true
+      # t.index [:test_id, :user_id]
+    end
   end
 end
