18b7396625e7203817724e02152f4ffbde07d1b8
eugeneshumilin <shumilinea@gmail.com>
2018-03-05 16:33:58 +0300

    rescue exception

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = @test.questions
@@ -33,5 +34,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
   end
 end
