01629e1894d4bbda6f29eb1b3395fd2085326210
eugeneshumilin <shumilinea@gmail.com>
2018-03-07 15:42:46 +0300

    create tests_controller

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,43 @@
+class TestsController < ApplicationController
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.save
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+end
