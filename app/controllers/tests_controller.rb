ec9112294f8a732560f51c4fca506aa385580960
eugeneshumilin <shumilinea@gmail.com>
2018-03-07 17:40:06 +0300

    create form and error partials

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -38,6 +38,6 @@ class TestsController < ApplicationController
   private
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
