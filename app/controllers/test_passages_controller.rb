da3c481a55f0930e7da5e8013d8fd89aae0b271d
eugeneshumilin <shumilinea@gmail.com>
2018-03-12 13:52:48 +0300

    create answers and test_passages controllers

---
+++ app/controllers/test_passages_controller.rb
@@ -0,0 +1,11 @@
+class TestPassagesController < ApplicationController
+
+  def show
+  end
+
+  def result
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  resources :answers
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
+    end
+  end
 end
