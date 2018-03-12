10350f60f704b576aa77df7974d7a5439d61353b
eugeneshumilin <shumilinea@gmail.com>
2018-03-12 22:09:50 +0300

    the test passing logic implementation

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,7 +1,23 @@
 class TestPassagesController < ApplicationController
-  def show; end
+  before_action :set_test_passages, only: %i[show update result]
+
+  def show
+  end
 
   def result; end
 
-  def update; end
+  def update
+    @test_passage.accept!(params[:answer_ids])
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_test_passages
+    @test_passage = TestPassage.find(params[:id])
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,38 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+
+    self.current_question = next_question
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+
+    correct_answers_count == correct_answers.where(id: answer_ids).count &&
+      correct_answers_count == answer_ids.count
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 end
