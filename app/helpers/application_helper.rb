06bf6ed25db344899fa6369b945fd48a53db5e32
eugeneshumilin <shumilinea@gmail.com>
2018-03-15 20:00:00 +0300

    add flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: :blank
   end
+
+  def flash_message(message)
+    content_tag :p, flash[message], class: 'flash alert' if flash[message]
+  end
 end
