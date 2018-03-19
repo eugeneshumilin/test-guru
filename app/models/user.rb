c22bbbaec5653e019780df21aa4814469bb15e4e
eugeneshumilin <shumilinea@gmail.com>
2018-03-19 23:32:34 +0300

    add devise in project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,10 @@
 class User < ApplicationRecord
-  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: 'Test', foreign_key: 'author_id'
-
-  validates :email, format: VALID_EMAIL, uniqueness: true
-
-  has_secure_password
-
-  def passed_tests_by_level(level)
-    tests.by_level(level)
-  end
 
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
