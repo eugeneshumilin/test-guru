cb8300e3c2379d2e55fcc36d9983dd899087c26c
eugeneshumilin <shumilinea@gmail.com>
2018-03-20 00:28:22 +0300

    create admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319192436) do
+ActiveRecord::Schema.define(version: 20180319211705) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
@@ -75,9 +75,13 @@ ActiveRecord::Schema.define(version: 20180319192436) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
 end
