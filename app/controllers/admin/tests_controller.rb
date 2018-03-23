96f1a6700ebbf0b853cfa29fc920749ca9571a9d
eugeneshumilin <shumilinea@gmail.com>
2018-03-23 11:13:25 +0300

    add localization to project

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -19,7 +19,7 @@ class Admin::TestsController < Admin::BaseController
     @test = current_user.own_tests.build(test_params)
 
     if @test.save
-      redirect_to [:admin, @test]
+      redirect_to [:admin, @test], notice: t('.success')
     else
       render :new
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
   protect_from_forgery with: :exception
+
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
 
   private
 
@@ -11,5 +18,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     resource.admin? ? admin_tests_path : super
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
