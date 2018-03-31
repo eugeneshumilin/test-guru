34cd96241ff03c11698bc81a3063893161d29607
eugeneshumilin <shumilinea@gmail.com>
2018-03-31 12:23:31 +0300

    change sign in flash

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     super
-    set_flash_message(:notice, :signed_in_hello, fullname: current_user.full_name) if current_user.admin?
+    if current_user.admin? || current_user.full_name.size > 1
+      set_flash_message(:notice, :signed_in_hello, fullname: current_user.full_name)
+    else
+      set_flash_message(:notice, :hello_email, email: current_user.email)
+    end
   end
 end
