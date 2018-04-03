4d5d083e156650d20d07bf85047464661281b9ac
eugeneshumilin <shumilinea@gmail.com>
2018-04-03 20:44:47 +0300

    hotfix

--- app/assets/javascripts/utilities/checkpass.js
+++ app/assets/javascripts/utilities/checkpass.js
@@ -1,24 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
-  var password_confirmation = document.getElementById('user_password_confirmation')
+  var passwordConfirmation = document.getElementById('user_password_confirmation');
 
-  password_confirmation.addEventListener('input', comparePassword)
+  if (passwordConfirmation) { passwordConfirmation.addEventListener('input', comparePassword); }
 })
 
 function comparePassword() {
-  var pass1 = document.getElementById('user_password')
-  var pass2 = document.getElementById('user_password_confirmation')
+  var pass1 = document.getElementById('user_password');
+  var pass2 = document.getElementById('user_password_confirmation');
 
-  var goodColor = "#66cc66"
-  var badColor = "#ff6666"
+  var goodColor = "#66cc66";
+  var badColor = "#ff6666";
 
   if (pass2.value == '') {
-      pass1.style.backgroundColor = ''
-      pass2.style.backgroundColor = ''
+      pass1.style.backgroundColor = '';
+      pass2.style.backgroundColor = '';
   } else if(pass1.value == pass2.value) {
-      pass1.style.backgroundColor = goodColor
-      pass2.style.backgroundColor = goodColor
+      pass1.style.backgroundColor = goodColor;
+      pass2.style.backgroundColor = goodColor;
   } else {
-      pass1.style.backgroundColor = badColor
-      pass2.style.backgroundColor = badColor
+      pass1.style.backgroundColor = badColor;
+      pass2.style.backgroundColor = badColor;
   }
 }
