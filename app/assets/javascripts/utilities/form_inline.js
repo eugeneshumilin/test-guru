1955c1a8bcf0bad9ec9d245686661867311884f9
eugeneshumilin <shumilinea@gmail.com>
2018-04-05 20:10:52 +0300

    form_inline tests edit

---
+++ app/assets/javascripts/utilities/form_inline.js
@@ -0,0 +1,35 @@
+document.addEventListener('turbolinks:load', function() {
+  $('.form-inline-link').on('click', formInlineLinkHandler);
+
+  var errors = document.querySelector('.resource-errors');
+
+  if (errors) {
+    var resourceId = errors.dataset.resourceId;
+    formInlineHandler(resourceId);
+  };
+});
+
+
+function formInlineLinkHandler(event) {
+  event.preventDefault();
+
+  var testId = this.dataset.testId;
+  formInlineHandler(testId);
+};
+
+function formInlineHandler(testId) {
+  var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]');
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]');
+  var $formInline = $('.form-inline[data-test-id="' + testId + '"]');
+
+  $formInline.toggle();
+  $testTitle.toggle();
+
+  if (link) {
+    if ($formInline.is(':visible')) {
+      link.textContent = 'Отмена';
+    } else {
+      link.textContent = 'Редактировать';
+    };
+  };
+};
--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,11 +1,10 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy start update_inline]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def show; end
 
@@ -35,6 +34,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
@@ -49,6 +56,10 @@ class Admin::TestsController < Admin::BaseController
 
   def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_tests
+    @tests = Test.all
   end
 
   def test_params
