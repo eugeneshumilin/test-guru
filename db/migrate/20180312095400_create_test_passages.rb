06227cbb15415d1406fe1649612bbfeeb5f0b838
eugeneshumilin <shumilinea@gmail.com>
2018-04-10 17:48:15 +0300

    change test_passages table

--- db/migrate/20180312095400_create_test_passages.rb
+++ db/migrate/20180312095400_create_test_passages.rb
@@ -3,7 +3,7 @@ class CreateTestPassages < ActiveRecord::Migration[5.1]
     create_table :test_passages do |t|
       t.references :user, foreign_key: true
       t.references :test, foreign_key: true
-      t.references :current_question, foreign_key: true
+      t.references :current_question, foreign_key: { to_table: :questions }
       t.integer :correct_questions, default: 0
 
       t.timestamps
