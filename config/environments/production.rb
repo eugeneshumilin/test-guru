5df6a2d7c38c0d4628b273b4aa8db76c03ff0b4f
eugeneshumilin <shumilinea@gmail.com>
2018-04-10 20:22:49 +0300

    rename host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'fierce-reef-21428.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'es-testguruapp.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
