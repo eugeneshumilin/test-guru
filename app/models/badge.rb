8da656f8b61b12725b84892abdf4adb909ba7dca
eugeneshumilin <shumilinea@gmail.com>
2018-04-18 23:08:10 +0300

    add validations to badge model

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,6 @@
 class Badge < ApplicationRecord
-  has_many :badge_users
+  has_many :badge_users, dependent: :destroy
   has_many :users, through: :badge_users
+
+  validates :image, :title, :rule_number, :rule_body, presence: true
 end
