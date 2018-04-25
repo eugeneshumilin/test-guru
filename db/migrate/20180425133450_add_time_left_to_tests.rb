1befb6cffe28a0c6ecc1fb5b238c1bcf17c3ae05
eugeneshumilin <shumilinea@gmail.com>
2018-04-25 16:59:05 +0300

    fix timeleft

--- db/migrate/20180425133450_add_time_left_to_tests.rb
+++ db/migrate/20180425133450_add_time_left_to_tests.rb
@@ -1,7 +1,5 @@
 class AddTimeLeftToTests < ActiveRecord::Migration[5.1]
   def change
-    def change
-      add_column :tests, :timeleft, :integer
-    end
+    add_column :tests, :timeleft, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20180425133450) do
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
     t.integer "author_id"
+    t.integer "timeleft"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
